4bb386cd3f6a706e63633eb0b42c85f0f19fd793
Moiz Ansari <moizansari.india@gmail.com>
2025-04-16 23:21:18 +0530

    BackendWithPrisma

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+// Coustom Router
+import userRouter from './routes/userRoutes';
+
+dotenv.config()
+const app = express();
+ 
+app.use(cookieParser)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.send("Comming From ROOT directroy")
+});
+
+app.use('/api/users', userRouter)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Backend is running on ${process.env.PORT}`);
+})
+
